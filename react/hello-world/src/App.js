2660cfe3b1c93fb7282592f3674284b36bb66083
julieCodes2 <julieann.zeiger@gmail.com>
2023-05-19 02:21:09 +0200

    Class-based component

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 //import Greet from "./components/Greet";
 import { Greet, Greet2 } from "./components/Greet";
+import Welcome from "./components/welcome";
 
 function App() {
   return (
     <div className="App">
       <Greet />
       <Greet2 />
+      <Welcome />
     </div>
   );
 }
