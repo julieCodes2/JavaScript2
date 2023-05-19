3b637ab00930ded6136b5a929af56603db65e078
julieCodes2 <julieann.zeiger@gmail.com>
2023-05-19 02:04:41 +0200

    Creating Components

---
+++ react/hello-world/src/components/Greet.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Greet() {
+    return (
+        <div>
+            <h1>Hello Julie</h1>
+        </div>
+    );
+}
+ const Greet2 = () => {
+    return (
+      <div>
+            <h1>Hello Julie Again</h1>
+         </div>  
+    );
+ };
+export {Greet, Greet2};
+
