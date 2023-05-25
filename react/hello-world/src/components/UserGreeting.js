34abe6f53c6ef0b5a1c8e9631074961f157c176f
julieCodes2 <julieann.zeiger@gmail.com>
2023-05-25 20:45:23 +0200

    4 approaches to conditionally render UI in react

--- react/hello-world/src/components/UserGreeting.js
+++ react/hello-world/src/components/UserGreeting.js
@@ -4,21 +4,37 @@ class UserGreeting extends Component {
     constructor(props){
         super(props);
         this.state={
-            isLoggedIn:false,
+            isLoggedIn:true,
         };
     }
   render() {
+    // 4 approaches to conditionally render UI in react, ef else; element variables; conditional ternary operator; and the short circuit operator; conditonal and short circuit operator most recommended
     // if(this.state.isLoggedIn){
     //     return <h1>Welcome Julie! Logged in.</h1>;
     // } else {
     //     return <h1>Welcome Guest. Logged out.</h1>;
-    let message;
-    if(this.state.isLoggedIn){
-        message = <div>your message here</div>
-        return <div>{message}</div>;
-    
+
+    //start commenting out the second approach
+    // let message;
+    // if(this.state.isLoggedIn){
+    //     message = <div>Welcome Julie</div>
+        
+    // } else {
+    //     message = <div>Welcome Guest</div>
+    // }
+    // return <div>{message}</div>
+    //end of commenting out the second approach
+
+    //now this one is the ternary operator approach. This is a simpler method
+    // return(
+    //     this.state.isLoggedIn ?
+    //     <div>Welcome Julie</div> :
+    //     <div> Welcome Guest</div>
+    // )
+    //the fourth approach called the circuit operator approach. When you want to render either something or nothing. 
+    return this.state.isLoggedIn && <div>Welcome Julie</div>
     }
     }
-}
+
 
 export default UserGreeting;
